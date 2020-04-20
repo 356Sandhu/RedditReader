b446c30485106cb4516269047f42a08978c4a6b8
Satvir Sandhu <356Sandhu@gmail.com>
2020-04-20 15:41:20 -0400

    add createUtterances

--- app.js
+++ app.js
@@ -5,32 +5,34 @@ let script = new SpeechSynthesisUtterance(
   "Hello My Friend, How are you? . ! . I hope you are doing well. ! Isn't it an exciting time to be alive?"
 );
 
+let script1 = new SpeechSynthesisUtterance("Welcome to Iceland.");
+
 const getPosts = async () => {
   const res = await fetch("https://www.reddit.com/.json");
   if (res.ok) console.log("response successful");
   else console.log("response failed");
+
   const json = await res.json();
   const data = json.data.children;
-  // data.forEach((item) => {
-  //   console.log(item.data.title);
-  // });
 
   return data;
 };
 
-// const doShit = async () => {
+const createUtterances = (data) => {
+  let utterances = [];
+  data.forEach((post) => {
+    const title = post.data.title;
+    const utterance = new SpeechSynthesisUtterance(title);
+    utterances.push(utterance);
+  });
+  return utterances;
+};
+
+// const doStuff = async () => {
 //   const data = await getPosts();
-
-//   data.forEach((item) => {
-//     console.log(`TITLE: ${item.data.title}`);
-//   });
-
-//   let randomPost = data[1].data.title;
-
+//   let utterances = createUtterances(data);
+//   console.log(utterances);
+//   return utterances;
 // };
 
-// doShit();
-
-btn.addEventListener("click", () => {
-  synth.speak(script);
-});
+btn.addEventListener("click", () => {});
